7e306304f545b25115e6f75b26670eb395691cdb
Chirack96 <jacques-chirack.gougou@epitech.eu>
2024-03-07 17:26:25 +0100

    Refactor styling in app.component.scss, app.component.html, addproduct.component.scss, panier.component.html, header.component.ts, and header.component.scss

--- Frontend/onlineproducts/src/app/addproduct/addproduct.component.ts
+++ Frontend/onlineproducts/src/app/addproduct/addproduct.component.ts
@@ -1,81 +1,74 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import axios from 'axios';
-import { Products } from '../models/product.model';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzNotificationModule, NzNotificationService } from 'ng-zorro-antd/notification';
 import { AuthService } from '../services/auth.services';
-import { AxiosService } from '../services/axios.service';
 import { ProductsService } from '../services/product.services';
+
 @Component({
   selector: 'app-addproduct',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule, NzNotificationModule],
   templateUrl: './addproduct.component.html',
-  styleUrl: './addproduct.component.scss'
+  styleUrls: ['./addproduct.component.scss']
 })
-export class AddproductComponent implements OnInit{
-  products: Products[] = [];
-  authService: AuthService = new AuthService();
-
-
-  newProduct = {title: '', description: '', imageUrl: '', price: 0, quantity: 0};
+export class AddproductComponent implements OnInit {
+  validateForm!: FormGroup;
   selectedFile: File | null = null;
-  constructor(@Inject(Router) private router: Router, private productsService: ProductsService, private axiosService: AxiosService) {}
-  async ngOnInit(): Promise<void> {
-    await this.loadProducts();
+
+  constructor(@Inject(Router) private router: Router, private fb: FormBuilder, private productsService: ProductsService, private authService: AuthService, private notification: NzNotificationService) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      price: [null, Validators.required],
+      quantity: [null, Validators.required],
+    });
   }
-  onFileSelected(event: any) {
+
+  onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
   }
-  async onSubmit() {
+
+  async onSubmit(): Promise<void> {
+    if (!this.validateForm.valid) {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity();
+        }
+      });
+      return;
+    }
+    this.notification.create('success', 'Product Added', 'Your product has been added successfully');
+
     const formData = new FormData();
     if (this.selectedFile) {
       formData.append('image', this.selectedFile, this.selectedFile.name);
     }
 
-    formData.append('title', this.newProduct.title);
-    formData.append('description', this.newProduct.description);
-    formData.append('price', this.newProduct.price.toString());
-    formData.append('quantity', this.newProduct.quantity.toString());
-    console.log(this.authService.isAuthenticated().toString());
+    Object.keys(this.validateForm.value).forEach(key => {
+      formData.append(key, this.validateForm.value[key]);
+    });
+
     try {
-      
       const token = localStorage.getItem('token');
-      console.log(token);
       if (!token) {
         console.error('Erreur lors de la récupération du token:', token);
         return;
       }
-      const headers = {
-        Authorization: `Bearer ${token}`,
-      };
-       
-        const response = await axios.post('http://localhost:3000/storeProduct', formData, { headers })
-        .then((response) => {
-        console.log(response);
-        return response;
-    }
-    );
-    
-        
-        // Réinitialiser le formulaire après l'envoi avec succès
-        this.newProduct = {title: '', description: '', imageUrl: '', price: 0, quantity: 0};
-
-        // Rafraîchir la liste des produits après l'ajout
-        await this.loadProducts();
+      const response = await axios.post('http://localhost:3000/storeProduct', formData, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+      console.log(response);
+      this.validateForm.reset();
     } catch (error) {
-        console.error('Erreur lors de l\'envoi des données au backend:', error);
+      console.error('Erreur lors de l\'envoi des données au backend:', error);
     }
-    
-}
-
-private async loadProducts(): Promise<void> {
-  console.log(this.authService.isAuthenticated().toString());
-    try {
-        const products = await this.productsService.getAllProducts();
-        this.products = products;
-    } catch (error) {
-        console.error('Erreur lors de la récupération des produits:', error);
-    }
-}
+  }
 }
--- Frontend/onlineproducts/src/app/header/header.component.ts
+++ Frontend/onlineproducts/src/app/header/header.component.ts
@@ -1,21 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-
 import { AuthService } from '../services/auth.services';
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     standalone: true,
-    imports: [RouterModule, CommonModule, NzAvatarModule, NzToolTipModule],
+    imports: [RouterModule, CommonModule, NzAvatarModule, NzToolTipModule, NzLayoutModule],
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
     test=new AuthService;
    
     constructor(private authService: AuthService,@Inject(Router) private router: Router) {}
+
+    ngOnInit(): void {
+    this.loadDarkModePreference();
+  }
 
     isLoggedIn(): boolean {
         return AuthService.isLoggedIn;
@@ -29,4 +34,14 @@ export class HeaderComponent {
   getAllProducts(): void {
     this.router.navigate(['/home']);
 }
+toggleDarkMode(): void {
+    const body = document.body;
+    body.classList.toggle('ant-dark');
+    localStorage.setItem('darkMode', body.classList.contains('ant-dark') ? 'enabled' : 'disabled');
+  }
+
+  private loadDarkModePreference(): void {
+    const darkModeEnabled = localStorage.getItem('darkMode') === 'enabled';
+    document.body.classList.toggle('ant-dark', darkModeEnabled);
+  }
 }
