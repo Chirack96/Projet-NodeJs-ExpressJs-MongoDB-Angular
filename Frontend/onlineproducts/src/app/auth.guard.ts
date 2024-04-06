d5a06a7d27bff9ca56add14a8ac885eee96e4e02
Chirack96 <jacques-chirack.gougou@epitech.eu>
2024-04-07 00:26:32 +0200

    fix authorizhation for routes

--- Frontend/onlineproducts/src/app/auth.guard.ts
+++ Frontend/onlineproducts/src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { AuthService } from './services/auth.services'; // Assurez-vous d'ajuster le chemin d'importation selon votre structure
 
 @Injectable({
@@ -8,11 +8,21 @@ import { AuthService } from './services/auth.services'; // Assurez-vous d'ajuste
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const requiresAdmin = route.data['requiresAdmin'] || false;
+    
+    // Vérifiez si l'utilisateur est authentifié
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login']);
       return false;
     }
+    
+    // Si la route nécessite un administrateur, vérifiez si l'utilisateur est un administrateur
+    if (requiresAdmin && !AuthService.isAdmin) {
+      this.router.navigate(['/not-authorized']);
+      return false;
+    }
+    
     return true;
   }
 }
