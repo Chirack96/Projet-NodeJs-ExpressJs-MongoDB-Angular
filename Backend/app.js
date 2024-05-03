9cf01e5c8b760407a6c1228285e3c73fbbd3b406
Chirack96 <jacques-chirack.gougou@epitech.eu>
2024-05-03 12:13:37 +0200

    chore: Update MongoDB connection string in app.js Creation of Docker's container for backend and frontend

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ const dbPass = process.env.DB_PASSWORD;
 
 
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.qiaejfh.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
